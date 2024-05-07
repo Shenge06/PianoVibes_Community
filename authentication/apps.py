385ac30e83121d5f4f67a1e0143c784f3139b307
Nkululeko Buthelezi <nkululekobuthelezi@Nkululekos-MacBook-Pro.local>
2024-05-07 12:56:32 +0200

    Added docstrings to my_function

--- authentication/apps.py
+++ authentication/apps.py
@@ -2,5 +2,14 @@ from django.apps import AppConfig
 
 
 class AuthenticationConfig(AppConfig):
+    """
+    AppConfig for the 'authentication' app.
+
+    This AppConfig is used to configure settings specific to the 'authentication' app.
+
+    Attributes:
+        default_auto_field (str): The default primary key field type for models in this app.
+        name (str): The name of the app ('authentication').
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
--- authentication/models.py
+++ authentication/models.py
@@ -2,10 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
+    """
+    A model representing a blog post.
+
+    Attributes:
+        user (ForeignKey): The user who created the post.
+        title (CharField): The title of the post.
+        content (TextField): The content of the post.
+        created_at (DateTimeField): The date and time when the post was created.
+    """
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Returns the title of the post.
+
+        Returns:
+            str: The title of the post.
+        """
         return self.title
+
